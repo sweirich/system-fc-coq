763c893321fffb148f717ad7b7da85c0e1517c0d
Stephanie Weirich <sweirich@cis.upenn.edu>
2015-06-11 22:05:45 -0400

    added kinds (except for one shifting lemma)

---
+++ coq/Weakening.v
@@ -0,0 +1,20 @@
+Require Import Omega.
+
+Require Export Typing.
+Require Export Coq.Logic.Decidable.
+
+Module WEAKENING.
+Import SYSTEMFC.
+Import SHIFTING.
+Import SUBSTITUTION.
+Import TYPING.
+
+(** This module contains various properties related to 
+    Weakening and Strengthening for the type system. 
+  
+*)
+
+
+(* ####################################################### *)  
+
+(* ####################################################### *)  
